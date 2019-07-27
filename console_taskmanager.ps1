8440f555437457b7405432a6635561f82b7d2fbe
unknown <infirmitive@gmail.com>
2019-07-27 13:22:06 +0300

    new version

--- console_taskmanager.ps1
+++ console_taskmanager.ps1
@@ -1,27 +1,29 @@
-function get-proctimeandRAM() {
+Function Get-CPUProcess {
     [CmdletBinding()]
     param (
         [string]$computername = $env:COMPUTERNAME
     )
-
-    Get-WmiObject -ClassName Win32_Process -ComputerName $computername |
-    foreach {
-        $props = [ordered]@{
-            Name          = $psitem.Name
-            ProcessId     = $psitem.ProcessId
-            RAM           = [math]::Round($psitem.WS / 1mb)
-            ProcessorTime = [math]::Round(($psitem.KernalModeTime + $psitem.UserModeTime) / 10000000)
-            UserName      = $psitem.GetOwner().Domain + "\" + $psitem.GetOwner().User
-        }
-        New-Object -TypeName PSObject -Property $props
-    }
+    $cores = (Get-WmiObject Win32_Processor -ComputerName $computername).NumberOfLogicalProcessors
+    $properties = @(
+        @{Name = "Name"; Expression = { $_.name } },
+        @{Name = "PID"; Expression = { $_.IDProcess } },
+        @{Name = "CPU (%)"; Expression = { for ($i = 0; $i -ne 3; $i++) { (Get-WMiObject -class Win32_PerfFormattedData_PerfProc_Process -ComputerName $computername -Filter "IDProcess='$($_.IDProcess)'").PercentProcessorTime / $cores; Start-Sleep(1)} } },
+        @{Name = "Memory (MB)"; Expression = { [Math]::Round(($_.workingSetPrivate / 1mb), 2) } },
+        @{Name = "Disk (MB)"; Expression = { [Math]::Round(($_.IODataOperationsPersec / 1mb), 2) } },
+        @{Name = "User"; Expression = { $_.PSComputerName + '\' + (Get-WMiObject Win32_Process -ComputerName $computername -Filter "ProcessId='$($_.IDProcess)'").GetOwner().User } }
+    )
+    $ProcessCPU = Get-WmiObject -class Win32_PerfFormattedData_PerfProc_Process -ComputerName $computername | Where-Object { $_.Name -notmatch "^(idle|_total|system)$" -and $_.PercentProcessorTime -gt 0 } |
+    Select-Object $properties |
+    Sort-Object $sortby -desc |
+    Select-Object -First 10 |
+    Format-Table -AutoSize
+    $ProcessCPU
 }
 
 $server = Read-Host 'Please enter PC Name'
-$sortby = 'ProcessorTime'
+$sortby = 'CPU (%)'
 $sort = Read-Host 'Choose sort mode (1 - by CPU (Default), 2 - by RAM)'
 If ($sort -eq '2') {
-    $sortby = 'RAM'
+    $sortby = 'Memory (MB)'
 }
-
-get-proctimeandRAM -ComputerName $server | Sort-Object -Descending $sortby | ft -AutoSize | Select -First 15
+Get-CPUProcess -computername $server
